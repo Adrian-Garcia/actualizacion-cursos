98a5cba7e110fd281c119e841f4fa097c94a92ad
Adrian Garcia <adriangarlop@hotmail.com>
2020-03-22 17:48:26 -0600

    Fixed Quiz 1

--- cursos/Curso de estadistica basica/Curso de estadistica basica/assets/js/quizes/quiz1-1.js
+++ cursos/Curso de estadistica basica/Curso de estadistica basica/assets/js/quizes/quiz1-1.js
@@ -1,47 +1,51 @@
-$('#check1').on('click', function() {
+$('#check').on('click', function() {
 
-	let responses = $('.quest1');
+	let responses1 = $('.quest1');
+	let responses2 = $('.quest2');
+	let responses3 = $('.quest3');
 
-	for (let i=0; i<responses.length; i++) {
+	for (let i=0; i<responses1.length; i++) {
 		
-		if (responses[i].checked && responses[i].value == 'correct') {
-			alert('¡Correcto!');
+		if (responses1[i].checked && responses1[i].value == 'correct') {
+			$("#response1").empty();
+			$("#response1").append("¡Respuesta correcta!");
+			$("#response1").css('color', 'green');
 		}
 
-		else if (responses[i].checked) {
-			alert('Incorrecto');
+		else if (responses1[i].checked) {
+			$("#response1").empty();
+			$("#response1").append("Respuesta incorrecta");
+			$("#response1").css('color', 'red');
 		}
 	}
-});
 
-$('#check2').on('click', function() {
-
-	let responses = $('.quest2');
-
-	for (let i=0; i<responses.length; i++) {
+	for (let i=0; i<responses2.length; i++) {
 		
-		if (responses[i].checked && responses[i].value == 'correct') {
-			alert('¡Correcto!');
+		if (responses2[i].checked && responses2[i].value == 'correct') {
+			$("#response2").empty();
+			$("#response2").append("¡Respuesta correcta!");
+			$("#response2").css('color', 'green');
 		}
 
-		else if (responses[i].checked) {
-			alert('Incorrecto');
+		else if (responses2[i].checked) {
+			$("#response2").empty();
+			$("#response2").append("Respuesta incorrecta");
+			$("#response2").css('color', 'red');
 		}
 	}
-});
 
-$('#check3').on('click', function() {
-
-	let responses = $('.quest3');
-
-	for (let i=0; i<responses.length; i++) {
+	for (let i=0; i<responses3.length; i++) {
 		
-		if (responses[i].checked && responses[i].value == 'correct') {
-			alert('¡Correcto!');
+		if (responses3[i].checked && responses3[i].value == 'correct') {
+			$("#response3").empty();
+			$("#response3").append("¡Respuesta correcta!");
+			$("#response3").css('color', 'green');
 		}
 
-		else if (responses[i].checked) {
-			alert('Incorrecto');
+		else if (responses3[i].checked) {
+			$("#response3").empty();
+			$("#response3").append("Respuesta incorrecta");
+			$("#response3").css('color', 'red');
 		}
 	}
-});
+})
