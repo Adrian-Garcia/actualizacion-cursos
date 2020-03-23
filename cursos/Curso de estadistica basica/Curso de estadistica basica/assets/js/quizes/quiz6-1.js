ee926f1deec3e49848c5fef0e09cb8169b6712b0
Adrian Garcia <adriangarlop@hotmail.com>
2020-03-23 10:53:51 -0600

    Finished quizes

--- cursos/Curso de estadistica basica/Curso de estadistica basica/assets/js/quizes/quiz6-1.js
+++ cursos/Curso de estadistica basica/Curso de estadistica basica/assets/js/quizes/quiz6-1.js
@@ -1,16 +1,19 @@
-$('#check0').on('click', function() {
+$('#check').on('click', function() {
 
-	let responses = $('.quest0');
+	let responses1 = $('.quest0');
 
-	for (let i=0; i<responses.length; i++) {
-
-		if (responses[i].checked && responses[i].value == '1') {
-			alert('¡Correcto!');
+	for (let i=0; i<responses1.length; i++) {
+		
+		if (responses1[i].checked && responses1[i].value == '1') {
+			$("#response1").empty();
+			$("#response1").append("¡Respuesta correcta!");
+			$("#response1").css('color', 'green');
 		}
 
-		else if (responses[i].checked) {
-			alert('Incorrecto');
+		else if (responses1[i].checked) {
+			$("#response1").empty();
+			$("#response1").append("Respuesta incorrecta");
+			$("#response1").css('color', 'red');
 		}
 	}
 });
-
